9c1e7e1bac3bb2991b49c8b6d1f822c7ede757ba
WalterStern1 <fasterpermits@Fasters-MacBook-Pro-6.local>
2020-08-26 21:35:48 -0700

    running flask app

--- WS/app.py
+++ WS/app.py
@@ -1,6 +1,6 @@
-import flask
+import numpy as np
+from flask import Flask, request, jsonify, render_template
 import pickle
-import pandas as pd
 
 '''
 bath_entry = 4
@@ -11,32 +11,23 @@ year_built_entry = 1920
 latitude_entry = 45.499533
 longitude_entry = -122.675113
 '''
-with open(f'model/finalized_model.sav', 'rb') as f:
-    model = pickle.load(f)
+app = Flask(__name__)
+model = pickle.load(open('model/model.pkl', 'rb'))
 
-app = flask.Flask(__name__, template_folder='templates')
+@app.route('/')
+def home():
+    return render_template('main.html')
 
-@app.route('/', methods=['GET', 'POST'])
-def main():
-    if flask.request.method == 'GET':
-        return(flask.render_template('main.html'))
-    if flask.request.method == 'POST':
-        baths = flask.request.form['baths']
-        beds = flask.request.form['beds']
-        square_feet = flask.request.form['square_feet']
-        latitude = flask.request.form['latitude']
-        longitude = flask.request.form['longitude']
-        input_variables = pd.DataFrame([[baths, beds, square_feet, latitude, longitude]],
-                                       columns=['BATHS', 'BEDS', 'SQUARE FEET', 'LATITUDE', 'LONGITUDE'],
-                                       dtype=float)
-        prediction = model.predict(input_variables)[0]
-        return flask.render_template('main.html',
-                                     original_input={'Baths':baths,
-                                                     'Beds':beds,
-                                                     'Square Feet':square_feet,
-                                                     'Latitude':latitude,
-                                                     'Longitude':longitude},
-                                     result=prediction,
-                                     )
-if __name__ == '__main__':
-    app.run()
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = prediction.round(decimals=2)
+
+    return render_template('main.html', prediction_text='Price should be $ {}'.format(output))
+
+if __name__ == "__main__":
+    app.run(debug=True)
