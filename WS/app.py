31cb677b574ae8e63b31987f5349b92e1915d489
WalterStern1 <fasterpermits@Fasters-MacBook-Pro-6.local>
2020-08-29 13:50:29 -0700

    final flask commit

--- WS/app.py
+++ WS/app.py
@@ -2,15 +2,6 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 
-'''
-bath_entry = 4
-bed_entry = 4
-square_feet_entry = 2580
-lot_size_entry = 2613
-year_built_entry = 1920
-latitude_entry = 45.499533
-longitude_entry = -122.675113
-'''
 app = Flask(__name__)
 model = pickle.load(open('model/model.pkl', 'rb'))
 
@@ -28,6 +19,10 @@ def predict():
     output = prediction.round(decimals=2)
 
     return render_template('main.html', prediction_text='Bleep bloop... home price predicted to be ${}'.format(output))
+
+@app.route('/visualizations')
+def visualizations():    
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
